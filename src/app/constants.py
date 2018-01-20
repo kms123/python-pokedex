fb17470716a23fac2194b4e801609f7f5d2b9d93
Kyle Sheasby <kyle.sheasby@gmail.com>
2018-01-20 17:50:40 -0600

    Add credential checking on the api.

---
+++ src/app/constants.py
@@ -0,0 +1,3 @@
+API_USER_KEY_ASSOCIATIONS = {
+    'default': 'd64a0132-8b00-4d4a-a65e-a8b7bbcb5f9c',
+}
--- src/app/views/api/v1/pokemon.py
+++ src/app/views/api/v1/pokemon.py
@@ -1,10 +1,27 @@
+import json
+
 import webapp2
+
+from app.constants import API_USER_KEY_ASSOCIATIONS
 
 
 class GetPokemonByNumberHandler(webapp2.RequestHandler):
     def get(self):
+        self.check_credentials()
         response = {
             'message': 'Pokemon repsonse',
         }
         self.response.headers['Content-Type'] = 'application/json'
-        self.response.write(response)
+        self.response.write(json.dumps(response))
+
+    def check_credentials(self):
+        api_user = self.request.get('apiUser')
+        if not api_user:
+            self.abort(400, 'apiUser is required.')
+        api_key = self.request.get('apiKey')
+        if not api_key:
+            self.abort(400, 'apiKey is required.')
+
+        expected_key = API_USER_KEY_ASSOCIATIONS.get(api_user)
+        if not expected_key or not expected_key == api_key:
+            self.abort(403, 'Invalid apiUser/apiKey combination.')
