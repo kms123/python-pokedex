e45f69ea37e3c856ae51d67335faaf0b14be1b3e
Kyle Sheasby <kyle.sheasby@gmail.com>
2018-03-17 17:23:58 -0600

    Update vendor path for libs.

--- src/appengine_config.py
+++ src/appengine_config.py
@@ -3,4 +3,4 @@ import os
 from google.appengine.ext import vendor
 
 # Add any libraries installed in the "lib" folder.
-vendor.add(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'lib'))
+vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
