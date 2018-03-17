c750e478a98898cc14d93a01c812059547688dee
Kyle Sheasby <kyle.sheasby@gmail.com>
2018-03-16 18:58:15 -0600

    More pylint fixes.

--- src/app/models/pokemon.py
+++ src/app/models/pokemon.py
@@ -5,10 +5,16 @@ from google.appengine.ext import ndb
 class Pokemon(ndb.Model):
     """ The information pertaining to a specific pokemon """
     # reference https://cloud.google.com/appengine/docs/standard/python/ndb/
-    id = ndb.IntegerProperty(required=True)
+    id_number = ndb.IntegerProperty(required=True)
     name = ndb.StringProperty()
     created = ndb.DateTimeProperty(auto_now_add=True)
     updated = ndb.DateTimeProperty(auto_now=True)
 
-    def build_key(self, id):
-        return 'POKEMON-' + self.id
+    @staticmethod
+    def build_key(id_number):
+        """ Build the key for the entity """
+        return ndb.Key('POKEMON-{}'.format(id_number))
+
+    def get_by_id(self, id_number):
+        """ Get a pokemon model by pokemon ID number """
+        return self.build_key(id_number).get()
