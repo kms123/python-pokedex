e23838beaa17c50b4fa124d901f3d2c752f13fc1
Kyle Sheasby <kyle.sheasby@gmail.com>
2018-03-17 17:54:18 -0600

    DOn't actually have any dependencies to install in /lib yet.

--- src/appengine_config.py
+++ src/appengine_config.py
@@ -1,6 +1,6 @@
 """ Top-level app engine configs """
-import os
-from google.appengine.ext import vendor
+# import os
+# from google.appengine.ext import vendor
 
 # Add any libraries installed in the "lib" folder.
-vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
+# vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
