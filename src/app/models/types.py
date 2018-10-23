cd1b7b391c5249dfae2fa781380fa5f86a273279
Kyle Sheasby <kyle.sheasby@gmail.com>
2018-10-22 19:21:57 -0600

    Base type model.

---
+++ src/app/models/types.py
@@ -0,0 +1,32 @@
+import json
+
+from google.appengine.ext import ndb
+
+
+class Type(ndb.Model):
+    id_number = ndb.IndexProperty(required=True)
+    name = ndb.StringProperty()
+    created = ndb.DateTimeProperty(auto_now_add=True)
+    updated = ndb.DateTimeProperty(auto_now=True)
+
+    @staticmethod
+    def build_key(id_number):
+        """ Build the key for the entity """
+        return ndb.Key(Type, 'TYPE-{}'.format(id_number))
+
+    @classmethod
+    def get_by_id(cls, id_number):
+        """ Get a Type model by pokemon ID number """
+        return cls.build_key(id_number).get()
+
+    def to_json(self):
+        """ return a json representation of the object """
+        type = {
+            'name': self.name,
+            'id_number': self.id_number,
+            'updated': self.updated.strftime('%Y-%m-%d%H:%M:%S'),
+        }
+
+        return json.dumps(type)
+
+
